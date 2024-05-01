d1d25d2cacce36f731ccd72a3e5c918dd7d2b673
Brent Johnson <13071953+bljohnsondev@users.noreply.github.com>
2024-05-01 07:30:25 -0500

    Added ncdu alias for color theme

--- dotfiles/zsh/aliases.zsh
+++ dotfiles/zsh/aliases.zsh
@@ -4,7 +4,8 @@ alias sshcolor="TERM=xterm-256color ssh"
 alias updatearch="sudo pacman -Syu"
 alias cleandockerimages='sudo docker rmi $(sudo docker images --filter "dangling=true" -q --no-trunc)'
 alias runsshagent='eval "$(ssh-agent -s)"'
-alias diskutil='sudo ncdu --exclude=/mnt'
+alias ncdu="ncdu --color dark"
+alias diskutil='sudo ncdu --color dark --exclude=/mnt /'
 
 # now for the aliases that override common commands
 alias ls="eza"
