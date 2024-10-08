07b8b78c8ca6e5f164263baeb8e5cc0dcd8be4b4
Brent Johnson <13071953+bljohnsondev@users.noreply.github.com>
2024-10-08 08:09:41 -0500

    Fixed zen mode plugin config

--- dotfiles/nvim/lua/plugins/zenmode.lua
+++ dotfiles/nvim/lua/plugins/zenmode.lua
@@ -1,8 +1,10 @@
 return {
   "folke/zen-mode.nvim",
   opts = {
-    backdrop = 0.95,
-    width = 120,
-    height = 1,
+    window = {
+      backdrop = 0.95,
+      width = 140,
+      height = 1,
+    }
   }
 }
